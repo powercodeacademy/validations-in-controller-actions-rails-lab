359f4eb9f26f678398eaf48d4d648741bd326cf8
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-21 12:15:24 -0400

    Author validations

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,13 +4,22 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
+
+# Add validations to Author such that...
+# The name cannot be blank
+# The e-mail is unique
 
   private
 
